9c7259ba628f834c3bc3bf61b061076fd0f71e1a
christiaanjohns <christiaanjohns@gmail.com>
2018-12-20 10:41:19 -0700

    Update to use MOCK Util tools

--- PlaceDescriptorServiceTest.cpp
+++ PlaceDescriptorServiceTest.cpp
@@ -20,41 +20,48 @@ const string APlaceDescriptionService::ValidLatitude("38.005");
 const string APlaceDescriptionService::ValidLongitude("-104.44");
 
 //Stub Class: returns hard coded value
-// Mock class to return info without having to connect to service, for testing purposes.
 class HttpStub: public Http
 {
 public:
-    std::string returnResponse;
-    std::string expectedURL;
-    void initialize() override {}
-
-    std::string get(const std::string& url)const override
-    {
-        verify(url);
-        return returnResponse;//
-    }
-
-    void verify(const std::string& url)const
-    {
-        ASSERT_THAT(url, EndsWith(expectedURL));
-    }
+    // MOCK_METHOD0(,): 0 for 0 input params
+    //      firstParam: method name
+    //      secondParam: method type
+    MOCK_METHOD0(initialize, void());
+    MOCK_CONST_METHOD1(get, string(const string&));
 };
 
 TEST_F(APlaceDescriptionService, RetrunDescriptionForValidLocation)
 {
     HttpStub httpStub;  //test double
-    httpStub.returnResponse = R"({ "address": {
-//            "road":"Drury Ln",
-//            "city":"Fountain",
-//            "state":"CO",
-//            "country":"US"} } )";
     string urlStart(
             "http://open.mapquestapi.com/nominatim/v1/reverse?format=json&");
     string expectedURL(urlStart +
-                    "lat=" + APlaceDescriptionService::ValidLatitude + "&" +
-                    "lon=" + APlaceDescriptionService::ValidLongitude);
+                       "lat=" + APlaceDescriptionService::ValidLatitude + "&" +
+                       "lon=" + APlaceDescriptionService::ValidLongitude);
+    EXPECT_CALL(httpStub, get(expectedURL));
+
     PlaceDescriptionService service(&httpStub);
 
-    auto description = service.summaryDescription(ValidLatitude, ValidLongitude);
-    ASSERT_THAT(description, Eq("Drury Ln, Fountain, CO, US"));
+    service.summaryDescription(ValidLatitude, ValidLongitude);
 }
+
+//Stub Class: returns hard coded value
+// Mock class to return info without having to connect to service, for testing purposes.
+//class HttpStub: public Http
+//{
+//public:
+//    std::string returnResponse;
+//    std::string expectedURL;
+//    void initialize() override {}
+//
+//    std::string get(const std::string& url)const override
+//    {
+//        verify(url);
+//        return returnResponse;//
+//    }
+//
+//    void verify(const std::string& url)const
+//    {
+//        ASSERT_THAT(url, EndsWith(expectedURL));
+//    }
+//};
